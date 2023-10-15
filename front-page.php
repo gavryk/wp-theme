659b28c06d5f43db9f128ba0ff9dbd6c703d1f89
gavryk <gvozdyk23@gmail.com>
2023-10-15 16:14:51 +0300

    some changes

--- front-page.php
+++ front-page.php
@@ -1,10 +1,12 @@
 <?php get_header();
 global $post; ?>
 <section id="content">
-    <div class="wrap">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-        <?php endwhile; endif; ?>
-    </div>
+	<div class="container row">
+		<?php if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>
+	</div>
 </section>
 <?php get_footer(); ?>
