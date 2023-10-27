c501fbbb4e58068ff435cc795a94a2c0c158a221
gavryk <gvozdyk23@gmail.com>
2023-10-27 13:13:20 +0300

    fix: default page

--- page.php
+++ page.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
-<div id="content" class="container row page_id_<?php the_ID() ?>">
-	<main>
-		<h1 class="page-title">
-			<?php the_title(); ?>
-		</h1>
-		<div class="wysiwyg">
-			<?php if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post();
-					the_content();
-				endwhile;
-			endif;
-			?>
-		</div>
-	</main>
+<div class="default-page-content page_id_<?php the_ID() ?> m-space-md">
+	<div class="row">
+		<main>
+			<h1 class="page-title">
+				<?php the_title(); ?>
+			</h1>
+			<div class="wys">
+				<?php if ( have_posts() ) :
+					while ( have_posts() ) :
+						the_post();
+						the_content();
+					endwhile;
+				endif;
+				?>
+			</div>
+		</main>
+	</div>
 </div>
 <?php get_footer(); ?>
